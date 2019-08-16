75dcd5ce02958f41be0c17cebdd1a9926f022773
SAMAEL NEWGATE <SAMNEWGA@uat.edu>
2019-08-15 18:22:58 -0700

    presentation version v1

--- flasktest.py
+++ flasktest.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for
+import movements
 
-#import movements
-
+#Uncomment after debugging of ml.py
+#import ml
 
 app = Flask(__name__)
 
@@ -10,36 +11,37 @@ app = Flask(__name__)
 def home():
     return render_template('layout.html')
 
-@app.route('/controls')
-def controls():
-    return render_template('controls.html')
-
-@app.route('/action1', methods=['GET', 'POST'])
+@app.route('/action1')
 def action_one():
-
-    #movements.forward()
+    movements.forward()
     return render_template('controls.html')
 
 @app.route('/action2')
 def action_two():
-    #movements.backward()
+    movements.backward()
     return render_template('controls.html')
 
 @app.route('/action3')
 def action_three():
-    #movements.turn()
+    movements.turn()
     return render_template('controls.html')
 
 @app.route('/action4')
 def action_four():
-    #movements.turn2()
+    movements.turn2()
     return render_template('controls.html')
 
+# Place holder function for ML
 @app.route('/action5')
 def action_five():
-    print('hello')
+    #uncomment after debugging of ml.py
+    #ml.photo()
     return render_template('controls.html')
 
+@app.route('/controls')
+def controls():
+    return render_template('controls.html')
 
+# Remove debug=Treue and change to IP of network to access remotely
 if __name__=='__main__':
     app.run(debug=True)
